bd758a0d7e4d5a13c9a482081ae78a47db7d7512
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2023-11-16 20:09:36 -0600

    finished crud for routines

--- routines/urls.py
+++ routines/urls.py
@@ -6,9 +6,10 @@ app_name = 'routine'
 
 urlpatterns = [
 
-    # path('', views.routines, name='routines'),
-    # path('<int:pk>/update/', views.update_routine, name='update_routine'),
+
     path('create/', views.CreateRoutineView.as_view(), name='create_routine'),
-    # path('<int:pk>/delete/', views.delete_routine, name='delete_routine'),
-    # path('<int:pk>/detail/', views.detail_routine, name='detail_routine'),
+    path('update/<int:pk>/', views.UpdateRoutine.as_view(), name='update_routine'),
+    path('', views.ListRoutine.as_view(), name='list_routine'),
+    path('delete/<int:pk>/', views.DeleteRoutine.as_view(), name='delete_routine'),
+
 ]
